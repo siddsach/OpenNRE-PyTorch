4d5e33e6a4e90d8b168ef8c1ba53e19655973eea
Cloud User <centos@ip-10-0-3-24.us-west-2.compute.internal>
2018-11-20 20:03:00 +0000

    add env setup

---
+++ setup_env.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/siddsach/OpenNRE-PyTorch
+cd OpenNRE-PyTorch
+
+sudo yum install centos-release-scl -y
+sudo yum install rh-python36 -y
+scl enable rh-python36 bash -y
+
+python3 -m virtualenv .env
+source .env/bin/activate
+sudo pip3 install -r requirements.txt
