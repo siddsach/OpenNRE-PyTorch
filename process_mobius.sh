af6726e8d02084987cd260c717cee8569f674512
Cloud User <centos@ip-10-0-3-37.us-west-2.compute.internal>
2018-12-13 17:21:08 +0000

    fix out name

--- process_mobius.sh
+++ process_mobius.sh
@@ -2,4 +2,4 @@ sudo rm -r /efs/sid/mobius_data/mimic/clean
 python process_mobius.py
 sudo mv output/ /efs/sid/mobius_data/mimic/
 python train.py
-aws s3 cp hyper.json s3://slurm-cluster/sidsachdeva/
+aws s3 cp results.json s3://slurm-cluster/sidsachdeva/
